e16d6819a706d865363f79a8f851ac0ee301f3f2
klaa <mouatazk79@gmail.com>
2024-12-26 16:34:56 +0100

    adding the EmptyEvent and completing the messages

---
+++ common/common-domain/src/main/java/com/klaa/order/system/domain/event/EmptyEvent.java
@@ -0,0 +1,11 @@
+package com.klaa.order.system.domain.event;
+
+public final class EmptyEvent implements DomainEvent<Void>{
+    public static final EmptyEvent INSTANCE=new EmptyEvent();
+    private EmptyEvent() {
+    }
+    @Override
+    public void fire() {
+
+    }
+}
