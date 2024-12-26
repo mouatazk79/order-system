37c0903a7ed68379ff23efd5bfe3b2b5cd4156ae
klaa <mouatazk79@gmail.com>
2024-12-26 14:31:56 +0100

    adding implementation of ports and teh needed dependencies

--- driver-service/driver-domain/driver-application-service/src/main/java/com/klaa/order/system/domain/mapper/DriverDataMapper.java
+++ driver-service/driver-domain/driver-application-service/src/main/java/com/klaa/order/system/domain/mapper/DriverDataMapper.java
@@ -1,8 +1,14 @@
 package com.klaa.order.system.domain.mapper;
 
+import com.klaa.order.system.domain.dto.DriverRequest;
+import com.klaa.order.system.domain.entity.Driver;
+import com.klaa.order.system.domain.entity.OrderApproval;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DriverDataMapper {
 
+
+    public OrderApproval driverRequestToOrderApproval(DriverRequest driverRequest) {
+    }
 }
