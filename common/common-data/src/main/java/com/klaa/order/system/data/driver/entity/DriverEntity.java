3d483876731a35c220c7fd7358a7540db9b71ffc
klaa <mouatazk79@gmail.com>
2024-12-27 18:26:38 +0100

    adding the common data for the driver

---
+++ common/common-data/src/main/java/com/klaa/order/system/data/driver/entity/DriverEntity.java
@@ -0,0 +1,4 @@
+package com.klaa.order.system.data.driver.entity;
+
+public class DriverEntity {
+}
---
+++ common/common-data/src/main/java/com/klaa/order/system/data/driver/repository/DriverJpaRepository.java
@@ -0,0 +1,4 @@
+package com.klaa.order.system.data.driver.repository;
+
+public interface DriverJpaRepository {
+}
