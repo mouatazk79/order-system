66116b57fed60bbda8689887514f0d9bc4a4ee13
klaa <mouatazk79@gmail.com>
2025-01-02 14:24:47 +0100

    adding the outbox messages

---
+++ order-service/order-domain/order-application-service/src/main/java/com/klaa/order/system/domain/order/service/domain/outbox/model/driver/DriverRequestPayload.java
@@ -0,0 +1,28 @@
+package com.klaa.order.system.domain.order.service.domain.outbox.model.driver;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.klaa.order.system.domain.order.service.domain.dto.create.PositionAddress;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.time.ZonedDateTime;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class DriverRequestPayload {
+    @JsonProperty
+    private String orderId;
+    @JsonProperty
+    private BigDecimal price;
+    @JsonProperty
+    private PositionAddress position;
+    @JsonProperty
+    private PositionAddress destination;
+    @JsonProperty
+    private ZonedDateTime createdAt;
+    @JsonProperty
+    private String driverOrderStatus;
+}
