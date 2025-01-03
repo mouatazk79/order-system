7a22874da0c9106b6cfa210dd786227a1f553c45
klaa <mouatazk79@gmail.com>
2025-01-03 10:44:03 +0100

    removing the publishers

--- order-service/order-domain/order-application-service/src/main/java/com/klaa/order/system/domain/order/service/domain/ports/output/publisher/driver/OrderCancelledRequestMessagePublisher.java
+++
@@ -1,8 +0,0 @@
-package com.klaa.order.system.domain.order.service.domain.ports.output.publisher.driver;
-
-
-import com.klaa.order.system.domain.order.service.domain.event.OrderCancelledEvent;
-import com.klaa.order.system.domain.event.publisher.DomainEventPublisher;
-
-public interface OrderCancelledRequestMessagePublisher extends DomainEventPublisher<OrderCancelledEvent> {
-}
--- order-service/order-domain/order-application-service/src/main/java/com/klaa/order/system/domain/order/service/domain/ports/output/publisher/payment/OrderCreatedPaymentRequestMessagePublisher.java
+++
@@ -1,8 +0,0 @@
-package com.klaa.order.system.domain.order.service.domain.ports.output.publisher.payment;
-
-
-import com.klaa.order.system.domain.order.service.domain.event.OrderCreatedEvent;
-import com.klaa.order.system.domain.event.publisher.DomainEventPublisher;
-
-public interface OrderCreatedPaymentRequestMessagePublisher extends DomainEventPublisher<OrderCreatedEvent> {
-}
