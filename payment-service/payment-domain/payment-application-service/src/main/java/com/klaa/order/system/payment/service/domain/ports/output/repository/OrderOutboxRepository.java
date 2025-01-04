4fd0df0ca734e5de3ffaac1a540449504af43a62
klaa <mouatazk79@gmail.com>
2025-01-04 15:57:28 +0100

    adding the payment outbox output ports

---
+++ payment-service/payment-domain/payment-application-service/src/main/java/com/klaa/order/system/payment/service/domain/ports/output/repository/OrderOutboxRepository.java
@@ -0,0 +1,22 @@
+package com.klaa.order.system.payment.service.domain.ports.output.repository;
+
+
+import com.klaa.order.system.domain.valueobjects.PaymentStatus;
+import com.klaa.order.system.outbox.OutboxStatus;
+import com.klaa.order.system.payment.service.domain.outbox.model.OrderOutboxMessage;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface OrderOutboxRepository {
+    OrderOutboxMessage save(OrderOutboxMessage orderOutboxMessage);
+
+    Optional<List<OrderOutboxMessage>> findByTypeAndOutboxStatus(String type, OutboxStatus status);
+
+    Optional<OrderOutboxMessage> findByTypeAndSagaIdAndPaymentStatusAndOutboxStatus(String type,
+                                                                                    UUID sagaId,
+                                                                                    PaymentStatus paymentStatus,
+                                                                                    OutboxStatus outboxStatus);
+    void deleteByTypeAndOutboxStatus(String type, OutboxStatus status);
+}
