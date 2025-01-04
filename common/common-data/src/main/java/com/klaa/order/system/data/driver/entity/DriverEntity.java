510c49b21c2c8bb61c4016d0531e4b65a75984b9
klaa <mouatazk79@gmail.com>
2025-01-04 21:57:05 +0100

    adding the logic to the common data

--- common/common-data/src/main/java/com/klaa/order/system/data/driver/entity/DriverEntity.java
+++ common/common-data/src/main/java/com/klaa/order/system/data/driver/entity/DriverEntity.java
@@ -1,4 +1,26 @@
 package com.klaa.order.system.data.driver.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class DriverEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID driverId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String car;
+    private String phoneNumber;
+    private boolean active;
 }
--- common/common-data/src/main/java/com/klaa/order/system/data/driver/repository/DriverJpaRepository.java
+++ common/common-data/src/main/java/com/klaa/order/system/data/driver/repository/DriverJpaRepository.java
@@ -1,4 +1,10 @@
 package com.klaa.order.system.data.driver.repository;
 
-public interface DriverJpaRepository {
+import com.klaa.order.system.data.driver.entity.DriverEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+@Repository
+public interface DriverJpaRepository extends JpaRepository<DriverEntity, UUID> {
 }
