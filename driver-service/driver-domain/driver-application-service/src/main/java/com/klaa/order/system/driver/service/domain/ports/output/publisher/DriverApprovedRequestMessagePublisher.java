0f3e4177ca1b6aeb08e15bdf233f8e84d54d77b5
klaa <mouatazk79@gmail.com>
2025-01-05 11:33:30 +0100

    removing the publishers

--- driver-service/driver-domain/driver-application-service/src/main/java/com/klaa/order/system/driver/service/domain/ports/output/publisher/DriverApprovedRequestMessagePublisher.java
+++
@@ -1,7 +0,0 @@
-package com.klaa.order.system.driver.service.domain.ports.output.publisher;
-
-import com.klaa.order.system.driver.service.domain.event.OrderDriverApprovedEvent;
-import com.klaa.order.system.domain.event.publisher.DomainEventPublisher;
-
-public interface DriverApprovedRequestMessagePublisher extends DomainEventPublisher<OrderDriverApprovedEvent> {
-}
--- driver-service/driver-domain/driver-application-service/src/main/java/com/klaa/order/system/driver/service/domain/ports/output/publisher/DriverRejectedRequestMessagePublisher.java
+++
@@ -1,7 +0,0 @@
-package com.klaa.order.system.driver.service.domain.ports.output.publisher;
-
-import com.klaa.order.system.driver.service.domain.event.OrderDriverRejectedEvent;
-import com.klaa.order.system.domain.event.publisher.DomainEventPublisher;
-
-public interface DriverRejectedRequestMessagePublisher extends DomainEventPublisher<OrderDriverRejectedEvent> {
-}
