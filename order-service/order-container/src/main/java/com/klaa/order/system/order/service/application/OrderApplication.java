3b862bf793aefc21cbb729ec2c906d9f9c3b10f6
klaa <mouatazk79@gmail.com>
2025-01-12 15:31:23 +0100

    fixing the OrderApplication and the application.yml

--- order-service/order-container/src/main/java/com/klaa/order/system/order/service/application/OrderApplication.java
+++ order-service/order-container/src/main/java/com/klaa/order/system/order/service/application/OrderApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories(basePackages = "com.klaa.order.system.order.service")
-@EntityScan(basePackages = "com.klaa.order.system.order.service")
-@SpringBootApplication(scanBasePackages = "com.klaa.order.system.order.service")
+@EnableJpaRepositories(basePackages = {"com.klaa.order.system.order.service.data","com.klaa.order.system.data"})
+@EntityScan(basePackages = {"com.klaa.order.system.order.service.data","com.klaa.order.system.data"})
+@SpringBootApplication(scanBasePackages = "com.klaa.order.system")
 public class OrderApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class,args);
